8f8cd4b3a3d4c646e1d00f61be15788ac5d18adc
Manuel de Jesus Caceres Mercado <mcaceres@oem-00138266.internal.inss.org>
2025-11-26 15:04:32 -0600

    lenguaje en tabulator y botones de accion

---
+++ src/public/js/tabulatorLang.js
@@ -0,0 +1,37 @@
+const LANG = {
+    locale: true, // activa idioma
+    langs: {
+        "es-es": {
+            "pagination": {
+                "first": "Primero",
+                "first_title": "Primera página",
+                "last": "Último",
+                "last_title": "Última página",
+                "prev": "Anterior",
+                "prev_title": "Página anterior",
+                "next": "Siguiente",
+                "next_title": "Página siguiente",
+                "page_size": "Mostrar"
+            },
+            "headerFilters": {
+                "default": "filtrar columna...",
+                "columns": {
+                    "nombre": "Buscar por nombre..."
+                }
+            },
+            "ajax": {
+                "loading": "Cargando...",
+                "error": "Error al cargar datos",
+            },
+            "groups": {
+                "item": "elemento",
+                "items": "elementos",
+            },
+            "data": {
+                "loading": "Cargando...",
+                "error": "Error",
+                "empty": "No se encontraron resultados",
+            }
+        }
+    }
+}
